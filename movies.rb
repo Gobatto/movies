b3b851c6721052b94daee10416b8d3d3fc6bb107
Raphael Gobatto Laranjeira <raphagobatto@hotmail.com>
2022-07-09 18:27:25 -0300

    Create movies.rb

---
+++ movies.rb
@@ -0,0 +1,27 @@
+movies = {
+  'godfather' => '5'
+}
+puts "What would you like to do? "
+choice = gets.chomp
+
+case choice
+  when "add"
+    puts "What movie would you like to add? "
+      title = gets.chomp
+    puts "What rating does the movie have? "
+      rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    if movies[title.to_sym] == nil
+      movies = [movies[title.to_sym]]
+    else 
+      puts "Movie already on the list!"
+    end
+  when "update"
+    puts "Updated!"
+  when "display"
+    puts "Movies!"
+  when "delete"
+    puts "Deleted!"
+  else
+    puts "Error!"
+end
